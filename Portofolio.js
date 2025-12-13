759dbb869ac9d51be3e2c90174deaebe7e1b8d4a
DestaAlthaafYudhistira <destaalthaaf@gmail.com>
2025-12-13 19:02:49 +0700

    adding_new_feature

--- Portofolio.js
+++ Portofolio.js
@@ -116,3 +116,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+// --- Tombol Kembali ke Atas ---
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add('visible');
+    } else {
+        backToTopButton.classList.remove('visible');
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
